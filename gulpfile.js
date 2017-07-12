8d294ebc837e96590afa8c5d5ffeb258badb8c84
j-veber <kovalsky.corp@gmail.com>
2017-07-12 16:18:52 +1000

    init commit

---
+++ gulpfile.js
@@ -0,0 +1,28 @@
+var gulp = require('gulp'),
+    sass = require('gulp-sass'),
+    pug = require('gulp-pug'),
+    livereload = require('gulp-livereload');
+
+gulp.task('hello', function() {
+    console.log('Привет, я таск!');
+});
+
+gulp.task('pug', function buildHTML() {
+    return gulp.src('app/views/index.pug')
+    .pipe(pug())
+    .pipe(gulp.dest('app/'))
+    .pipe(livereload())
+});
+
+gulp.task('scss', function () {
+    return gulp.src('app/scss/*.scss')
+    .pipe(sass())
+    .pipe(gulp.dest('app/css'))
+    .pipe(livereload())
+});
+
+gulp.task('watch', function () {
+    gulp.watch('app/scss/*.scss', ['scss']);
+    gulp.watch('app/views/*.pug', ['pug']);
+    livereload.listen();
+});

a89d8b6de6d06ec21f813e380a795e6fcbe7907d
j-veber <kovalsky.corp@gmail.com>
2017-07-14 17:34:16 +1000

    modal window - done

--- app/js/main.js
+++ app/js/main.js
@@ -1,10 +1,59 @@
-document.addEventListener('DOMContentLoaded', function()  {
-    console.log(2);
-    let modal_choice_view = document.querySelector("#modal-choice");
-    modal_choice_view.style.display = "block";
+document.addEventListener('DOMContentLoaded', onLoad);
 
-    let modal_close_btn = document.querySelector("#close");
-    modal_close_btn.addEventListener("click", () => {
-        modal_choice_view.style.display = "none";
+let userChoice = "";
+let modalChoiceView = document.querySelector("#modal-choice");
+let modalCloseBtn = document.querySelector("#close");
+let res = document.querySelector(".choice p");
+
+let cell = {
+    num: number,
+    empty: boolaen,
+    co: boolean
+};
+
+
+function onLoad() {
+    // modalChoiceView.style.display = "block";
+    modalChoiceView.style.display = "none" ;
+    modalCloseBtn.addEventListener("click", () => {
+        modalChoiceView.style.display = "none";
     });
-});
+
+}
+
+function displayUserChoice() {
+    console.log(userChoice);
+    res.style.fontSize = "1.5rem";
+    res.textContent = userChoice;
+}
+
+function setCross() {
+    userChoice = "cross";
+    modalChoiceView.style.display = "none";
+    displayUserChoice();
+}
+
+function setOval() {
+    userChoice = "oval";
+    modalChoiceView.style.display = "none";
+    displayUserChoice();
+}
+
+function setRandom() {
+    let r = Math.floor(Math.random() * 2);
+    if (r === 1) {
+        userChoice = "oval";
+    } else {
+        userChoice = "cross";
+    }
+    modalChoiceView.style.display = "none";
+    displayUserChoice();
+}
+
+function setMark(inputdata) {
+
+    currentId = event.currentTarget.id;
+    // event.currentTarget.backgroundColor = 'transparent';
+    event.currentTarget.css.background = 'url("../img/cross.png")';
+    console.log(event.currentTarget.background);
+}

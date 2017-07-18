63c695b4547a5e7985330e54e48e1344f37bfce4
j-veber <kovalsky.corp@gmail.com>
2017-07-18 18:06:18 +1000

    game ver_1.0

--- app/js/main.js
+++ app/js/main.js
@@ -1,9 +1,10 @@
-document.addEventListener('DOMContentLoaded', onLoad);
-
 let userChoice;
 let modalChoiceView = document.querySelector("#modal-choice");
+let resultWindow = document.querySelector("#modal-result");
 let modalCloseBtn = document.querySelector("#close");
 let res = document.querySelector(".choice p");
+let tableCell = document.querySelector(".blocks-wrapper");
+let resString = document.querySelector("#result-string");
 let iteration = 0;
 let completeLevel = false;
 
@@ -13,7 +14,7 @@ let completeLevel = false;
  * @param co - cross or oval. cross - true; oval - false.
  * @param cell is empty - true; else -false
  */
-var cells = [
+const cells = [
     {
         num: 0,
         empty: true,
@@ -61,29 +62,45 @@ var cells = [
     },
 ];
 
+document.addEventListener('DOMContentLoaded', onLoad);
+
+(function eventListeners() {
+    modalCloseBtn.addEventListener("click", () => {
+        hideModalResultWindow();
+    });
+
+    tableCell.onclick = function (event) {
+        let target = event.target.id;
+        console.log(target);
+        setMark(target);
+    };
+})();
 
 function onLoad() {
+   showModalChoiceView();
+}
+
+function showModalChoiceView() {
     modalChoiceView.style.display = "block";
-    // modalChoiceView.style.display = "none" ;
-    // modalCloseBtn.addEventListener("click", () => {
-    //     modalChoiceView.style.display = "none";
-    // });
-    // if (completeLevel === true) alert("YO");
+}
+
+function hideModalChoiceView() {
+    modalChoiceView.style.display = "none";
+}
+
+function showModalResultWindow() {
+    resultWindow.style.display = "block";
+}
+
+function hideModalResultWindow() {
+    resultWindow.style.display = "none";
 }
 
 /**
  * @description Отображение выбора пользователя в колонке справа
  */
 function displayUserChoice() {
-    if (userChoice) {
-        console.log("cross");
-        res.style.fontSize = "1.5rem";
-        res.textContent = "Крестик";
-    } else {
-        console.log("oval");
-        res.style.fontSize = "1.5rem";
-        res.textContent = "Нолик";
-    }
+    res.textContent = userChoice ? "Крестик" : "Нолик";
 }
 
 /**
@@ -91,16 +108,14 @@ function displayUserChoice() {
  */
 function setCross() {
     userChoice = true;
-    modalChoiceView.style.display = "none";
+    hideModalChoiceView();
     displayUserChoice();
 }
-
 function setOval() {
     userChoice = false;
-    modalChoiceView.style.display = "none";
+    hideModalChoiceView();
     displayUserChoice();
 }
-
 function setRandom() {
     let r = Math.floor(Math.random() * 2);
     if (r === 1) {
@@ -108,35 +123,97 @@ function setRandom() {
     } else {
         userChoice = "cross";
     }
-    modalChoiceView.style.display = "none";
+    hideModalChoiceView();
     displayUserChoice();
 }
 
 /**
- * @description Функция для установки кретика/нолика для пользователя и ПК
+ * @description Функция для установки крестика/нолика для пользователя и ПК
  * @param inputId - number. ИД ячейки, в которую нужно поставить метку.
  */
 function setMark(inputId) {
-
+    inputId = Number.parseInt(inputId);
     if (cells[inputId].empty && (completeLevel === false) && iteration <=9) {
         iteration++;
         cells[inputId].empty = false;
         cells[inputId].co = userChoice;
-        if (userChoice) {
-            event.currentTarget.style.background = 'url("img/cross.png")';
-            event.currentTarget.style.backgroundSize = 'cover';
-        } else {
-            event.currentTarget.style.background = 'url("img/oval.png")';
-            event.currentTarget.style.backgroundSize = 'cover';
-        }
+        let currentCell = document.getElementById(inputId);
+        currentCell.style.background = userChoice ? 'url("img/cross.png")' : 'url("img/oval.png")';
         createProgressElement(true, inputId);
-        if (checkLines(inputId) === true) exit();
-        setTimeout(pcTurn, 100);
+        if (checkLines(inputId) === true) {
+            exit(true);
+        }
+        if (iteration < 9 && completeLevel === false) setTimeout(pcTurn, 100);
+        if (iteration === 9 && completeLevel === false) exit();
     } else {
-        if (iteration > 9) exit();
-        else console.log("ячейка с номер " + (inputId + 1) + " занята");
+        console.log("ячейка с номер " + (inputId) + " занята");
     }
+}
 
+/**
+ * @description Ход компьютера
+ */
+function pcTurn() {
+    let pcChoice = createPCCell();
+    if (cells[pcChoice].empty) {
+        iteration++;
+        cells[pcChoice].empty = false;
+        cells[pcChoice].co = !userChoice;
+        let pcCell = document.getElementById(cells[pcChoice].num.toString());
+        if (userChoice) {
+            pcCell.style.background = 'url("img/oval.png")';
+            pcCell.style.backgroundSize = 'cover';
+        } else {
+            pcCell.style.background = 'url("img/cross.png")';
+            pcCell.style.backgroundSize = 'cover';
+        }
+        createProgressElement(false, pcChoice);
+        if (checkLines(pcChoice) === true) exit(false);
+    } else {
+        if (iteration >= 9) {
+            exit();
+        } else pcTurn();
+    }
+}
+
+/**
+ * @description проверяет нет ли одинаковых элементов в ряду/диагонали.
+ * Если есть, то заканчивает игру, сообщая кто победил.
+ * @param cellId - ИД ячейки, которую надо проверить
+ * @return {boolean} - результат - false - не прошла проверка; true - иначе
+ */
+function checkLines(cellId) {
+    let res = true;
+    switch (cellId){
+        case 4:
+            if (checkVerticalLine(cellId) === false && checkHorizontalLine(cellId) === false
+                && !checkMainLine(cellId) && !checkLeftLine(cellId)) {
+                res = false;
+            }
+            break;
+        case 0:
+        case 8:
+            if (checkVerticalLine(cellId) === false && checkHorizontalLine(cellId) === false
+                && !checkMainLine(cellId)) {
+                res = false;
+            }
+            break;
+        case 2:
+        case 6:
+            if (!checkVerticalLine(cellId) && !checkHorizontalLine(cellId) && !checkLeftLine(cellId)) {
+                res = false;
+            }
+            break;
+        case 1:
+        case 3:
+        case 5:
+        case 7:
+            if (!checkVerticalLine(cellId) && !checkHorizontalLine(cellId)) {
+                res = false;
+            }
+            break;
+    }
+    return res;
 }
 
 /**
@@ -157,17 +234,17 @@ function checkVerticalLine(cellId) {
     return res;
 }
 
+
 /***
  * @description проверка одинаковых символов в строке
  * @param cellId - ИД кликнутой ячейки
  * @return {boolean} - результат - false - не прошла проверка; true - иначе
  */
 function checkHorizontalLine(cellId) {
-    let columns = [0,3,6];
     let cellSign = cells[cellId].co;
     let currentRow = cellId;
     let res = true;
-    while (currentRow !== columns[0] || currentRow !== columns[1] || currentRow !== columns[2]) {
+    while (currentRow !== 0 && currentRow !== 3 && currentRow !== 6) {
         currentRow--;
     }
     for (let i = 0; i < 3 && res; i++) {
@@ -222,93 +299,56 @@ function checkLeftLine(cellId) {
 }
 
 /**
- * @description проверяет нет ли одинаковых элементов в ряду/диагонали.
- * Если есть, то заканчивает игру, сообщая кто победил.
- * @param cellId - ИД ячейки, которую надо проверить
- */
-function checkLines(cellId) {
-    let res = true;
-    switch (cellId){
-        case 4:
-            if (!checkVerticalLine(cellId) || !checkHorizontalLine(cellId) || !checkMainLine(cellId) || !checkLeftLine(cellId)) {
-                res = false;
-            }
-            break;
-        case 0:
-        case 8:
-            if (checkVerticalLine(cellId) === false || !checkHorizontalLine(cellId) || !checkMainLine(cellId)) {
-                res = false;
-            }
-            break;
-        case 2:
-        case 6:
-            if (!checkVerticalLine(cellId) || !checkHorizontalLine(cellId) || !checkLeftLine(cellId)) {
-                res = false;
-            }
-            break;
-        case 1:
-        case 3:
-        case 5:
-        case 7:
-            if (!checkVerticalLine(cellId) || !checkHorizontalLine(cellId)) {
-                res = false;
-            }
-            break;
-    }
-    return res;
-}
-
-/**
- * @description создание элемента в истории ходов.
+ * @description создает запись в истории ходов.
  * @param inputUser - true - user; false - PC
- * @param inputid - ячейка, в которой был сделан ход
+ * @param inputId - ячейка, в которой был сделан ход
  */
-function createProgressElement(inputUser, inputid) {
+function createProgressElement(inputUser, inputId) {
     let p = document.createElement('p');
     if (inputUser) {
-        p.innerHTML = "User: " + inputid;
+        p.innerHTML = "User: " + inputId;
     } else {
-        p.innerHTML = "PC: " + inputid;
+        p.innerHTML = "PC: " + inputId;
     }
     progress.appendChild(p);
 }
 
-
 /**
- * @description Ход компьютера
+ * @description Логика выбора ячейки для ПК
+ * @return {number} - ячейка, которая была выбрана
  */
-function pcTurn() {
-    let pcChoice = createPCCell();
-    if (cells[pcChoice].empty) {
-        iteration++;
-        cells[pcChoice].empty = false;
-        cells[pcChoice].co = !userChoice;
-        // console.log("pcTurn: ", pcChoice);
-        let pcCell = document.getElementById(cells[pcChoice].num.toString());
-        if (userChoice) {
-            pcCell.style.background = 'url("img/oval.png")';
-            pcCell.style.backgroundSize = 'cover';
-        } else {
-            pcCell.style.background = 'url("img/cross.png")';
-            pcCell.style.backgroundSize = 'cover';
-        }
-        createProgressElement(false, pcChoice);
-        if (checkLines(pcChoice) === true) exit();
-    } else {
-        if (iteration>=9) {
-            exit();
-        } else pcTurn();
-    }
-}
-
 function createPCCell() {
     let pcChoice = Math.floor(Math.random() * (9));
 
-    // console.log(cells[pcChoice].num, cells[pcChoice].empty);
     return pcChoice;
 }
 
-function exit() {
+/**
+ * @description завершение текущей игры и вывод победителя
+ * @param inputUser - победивший игрок
+ */
+function exit(inputUser) {
+    if (completeLevel) resString.textContent = "Ничья" ;
+    resString.textContent =  inputUser ? "Вы победили!" : "Вы проиграли.";
+    showModalResultWindow();
     completeLevel = true;
-    alert("YO");
+}
+
+function resetLevel() {
+    iteration = 0;
+    completeLevel = false;
+    for (let i = 0; i < cells.length; i++) {
+        cells[i].co = undefined;
+        cells[i].empty = true;
+    }
+    hideModalResultWindow();
+    let table = document.querySelectorAll(".block");
+    let progressList = document.querySelectorAll(".progress p");
+    for (let i = 0; i < table.length; i++) {
+        table[i].style.background = "#ada4a4";
+    }
+    for (let i = 0; i < progressList.length; i++) {
+        progressList[i].remove();
+    }
+    modalChoiceView.display = "block";
 }
